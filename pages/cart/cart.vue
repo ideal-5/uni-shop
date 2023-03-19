9b2af8ade682dc5b431b4d68d731f1cae0d1245a
ideal <ide_al@163.com>
2023-03-19 21:45:23 +0800

    完成了tabbar的开发

---
+++ pages/cart/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    cart
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
